dfc57e349f544079add29b75ebe43cfc744f1e07
Ricardo Bordim <rbordim@gmail.com>
2022-05-29 17:42:10 -0300

    Ajustado o Namespace para Maiusculo

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $app->config('debug', true);
 $app->get('/', function() {
     
 	// echo "OK";
-	$sql = new Hcode\db\Sql();
+	$sql = new Hcode\DB\Sql();
 
 	$results = $sql->select("SELECT * FROM tb_users");
 
