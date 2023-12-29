eaae4a3a57fe57740f63f8b0aa4760e8f2f64e82
Ricardo Bordim <rbordim@gmail.com>
2023-12-29 15:10:18 -0300

    Feito o SQL funcionar

--- index.php
+++ index.php
@@ -11,9 +11,11 @@ $app->config('debug', true);
 
 $app->get('/', function() {
     
-	$page = new Page();
-	$page->setTpl("index");
-
+	//$page = new Page();
+	//$page->setTpl("index");
+	$sql = new Hcode\DB\Sql();
+	$results = $sql->select("Select * from tb_users");
+	echo json_encode($results);
 });
 
 $app->run();
