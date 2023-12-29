b7251d3d33f4f6f869515b0c54a19092a470b7be
Ricardo Bordim <rbordim@gmail.com>
2023-12-29 15:47:57 -0300

    Atualizando a formatação

--- index.php
+++ index.php
@@ -11,11 +11,12 @@ $app->config('debug', true);
 
 $app->get('/', function() {
     
-	//$page = new Page();
-	//$page->setTpl("index");
-	$sql = new Hcode\DB\Sql();
-	$results = $sql->select("Select * from tb_users");
-	echo json_encode($results);
+	$page = new Page();
+	$page->setTpl("index");
+	
+	//$sql = new Hcode\DB\Sql();
+	//$results = $sql->select("Select * from tb_users");
+	//echo json_encode($results);
 });
 
 $app->run();
