2bc7e0ae884bc39d62b5a39ceaac23d98ebf14cd
Ricardo Bordim <rbordim@gmail.com>
2024-01-06 14:01:11 -0300

    Admin de Categorias

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 session_start();
 require_once "vendor/autoload.php";
 
+use \Hcode\Model\Category;
 use \Hcode\Model\User;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
@@ -197,6 +198,84 @@ $app->post("/admin/forgot/reset",function(){
     "footer" => false,
     ]);
     $page->setTpl("forgot-reset-success");
+
+});
+
+$app->get("/admin/categories",function()
+{
+    User::verifyLogin();
+
+    $categories = Category::listAll();
+    $page = new PageAdmin();
+    $page->setTpl("categories",[
+        'categories'=>$categories
+    ]);
+
+});
+
+$app->get("/admin/categories/create", function () {
+
+    User::verifyLogin();
+
+    $page = new PageAdmin();
+    $page->setTpl("categories-create");
+
+});
+
+$app->post("/admin/categories/create", function () {
+
+    User::verifyLogin();
+
+    $category = new Category();
+    $category->setData($_POST);
+    $category->save();
+
+    header("Location: /admin/categories");
+    exit;
+
+  
+
+});
+
+
+$app->get("/admin/categories/:idcategory/delete",function($idcategory)
+{
+    $category = new Category();
+    $category->get((int)$idcategory);
+    $category->delete();
+
+    header("Location: /admin/categories");
+    exit;
+
+
+});
+
+
+$app->get("/admin/categories/:idcategory", function ($idcategory) {
+    
+    $category = new Category();
+    $category->get((int) $idcategory);
+
+    $page = new PageAdmin();
+    $page->setTpl("categories-update",[
+        'category'=>$category->getValues()
+    ]);
+
+
+});
+
+
+$app->post("/admin/categories/:idcategory", function ($idcategory) {
+    
+    $category = new Category();
+    $category->get((int) $idcategory);
+
+    $category->setData($_POST);
+
+    $category->save();
+    header("Location: /admin/categories");
+    exit;
+
 
 });
 
